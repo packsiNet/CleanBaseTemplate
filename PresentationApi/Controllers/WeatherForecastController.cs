c876e10b21ec4cb1e8cc5f90e99ae5926f30a307
shahr <shahramoweisy@gmail.com>
2025-10-08 11:19:58 +0330

    First Commit

---
+++ PresentationApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+}
